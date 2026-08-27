ce1c4922c555711b83a09dba2929a5207d81ad95
ederfias97 <edergavioes97@gmail.com>
2026-08-27 20:47:24 -0300

    array e objetos

---
+++ projetos/todo/list/javascript/funcoes/array/objects.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome: 'Eder',
+    idade: 29,
+    profissão: "Logistico",
+    genero: "Masculino",
+    email:"edergavioes97@gmail.com",
+
+}
+
+pessoa.estadoCivil ="fudido"
+console.log(pessoa)
