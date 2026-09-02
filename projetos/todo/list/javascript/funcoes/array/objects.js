abca462c34640f39614f9bf943830299ef7b5830
ederfias97 <edergavioes97@gmail.com>
2026-09-01 21:53:50 -0300

    testando

---
+++ projetos/todo/list/javascript/funcoes/array/objects.js
@@ -0,0 +1,9 @@
+const pessoa = {
+    nome : "Eder",
+    idade: 29,
+    profissão:"logistico",
+    genero : "Masculuno",
+    email: "edergavioes97@gmail.com",
+
+}
+console.log(pessoa.nome)
---
+++ projetos/todo/list/javascript/script.js
@@ -0,0 +1,16 @@
+const btnAddTask = document.getElementById("btn-add-desk")
+const tbody = document.getElementById("list-task")
+
+btnAddTask.addEventListener =("click",function () {
+    const inputTask = document.getElementById("input_task")
+    const tr = document.createElement("tr")
+    const task td = document.createElement("td")
+
+    const btnEdit =document.createElement("button")
+btnEdit.innerHTML = "Editar"
+btnAddTask.Edit.classList.add("btn","btn-info","btn-sm")
+
+const  btnDone = document.createElement("button"
+ btnDone.innerHTML = "Concluir"
+)
+})
